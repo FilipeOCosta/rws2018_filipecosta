25db1550dde4cc8ed3a21f66f3d3dc1121fdc86e
Filipe <costa.filipe@ua.pt>
2018-03-02 09:56:16 +0000

    dia 2.1

--- player_filipecosta/src/player_filipecosta_node.cpp
+++ player_filipecosta/src/player_filipecosta_node.cpp
@@ -8,8 +8,8 @@
 #include "std_msgs/String.h"
 
 #include <rws2018_libs/team.h>
-
 #include <tf/transform_broadcaster.h>
+#include <sstream>
 
 using namespace std;
 
@@ -38,7 +38,8 @@ public:
         return setTeamName("blue");
         break;
       default:
-        cout << "wrong team index given. Cannot set team" << endl;
+        // cout << "wrong team index given. Cannot set team" << endl;
+        ROS_WARN("wrong team index given. Cannot set team");
         break;
     }
   }
@@ -53,7 +54,8 @@ public:
     }
     else
     {
-      cout << "cannot set team name to " << argin_team << endl;
+      // cout << "cannot set team name to " << argin_team << endl;
+      ROS_WARN("cannot set team name to ");
       return 0;
     }
   }
@@ -77,6 +79,10 @@ public:
   boost::shared_ptr<Team> green_team;
   boost::shared_ptr<Team> blue_team;
 
+  boost::shared_ptr<Team> my_team;
+  boost::shared_ptr<Team> my_preys;
+  boost::shared_ptr<Team> my_hunters;
+
   tf::TransformBroadcaster br;  // declare the broadcaster
 
   MyPlayer(string argin_name, string argin_team) : Player(argin_name)
@@ -85,7 +91,27 @@ public:
     green_team = boost::shared_ptr<Team>(new Team("green"));
     blue_team = boost::shared_ptr<Team>(new Team("blue"));
 
-    setTeamName(argin_team);
+    if (red_team->playerBelongsToTeam(name))
+    {
+      my_team = red_team;
+      my_preys = green_team;
+      my_hunters = blue_team;
+      setTeamName("red");
+    }
+    else if (green_team->playerBelongsToTeam(name))
+    {
+      my_team = green_team;
+      my_preys = blue_team;
+      my_hunters = red_team;
+      setTeamName("green");
+    }
+    else if (blue_team->playerBelongsToTeam(name))
+    {
+      my_team = blue_team;
+      my_preys = red_team;
+      my_hunters = green_team;
+      setTeamName("blue");
+    }
 
     printReport();
   }
@@ -93,7 +119,7 @@ public:
   void move()
   {
     tf::Transform transform;  // declare the transformation object
-    transform.setOrigin(tf::Vector3(2, 3, 0.0));
+    transform.setOrigin(tf::Vector3(-8, -8, 0.0));
     tf::Quaternion q;
     q.setRPY(0, 0, M_PI / 4);
     transform.setRotation(q);
@@ -102,12 +128,16 @@ public:
 
   void printReport()
   {
-    cout << "My name is " << name << "and my team is " << getTeamName() << endl;
+    // cout << "My name is " << name << "and my team is " << getTeamName() << endl;
+
+    ROS_INFO("My name is %s and my team is %s", name.c_str(), (getTeamName().c_str()));
+    // ROS_WARN("My name is %s and my team is %s", name.c_str(), (getTeamName().c_str()));
+    // ROS_ERROR("My name is %s and my team is %s", name.c_str(), (getTeamName().c_str()));
   }
 };
 }  // end of namespace
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   // std::string player_name = "filipecosta";
   // Creating an instance of class Player
@@ -119,16 +149,17 @@ int main(int argc, char** argv)
 
   ros::init(argc, argv, "filipecosta");
 
+  ros::NodeHandle n;
+
   rws_filipecosta::MyPlayer my_player("filipecosta", "green");
 
   if (my_player.red_team->playerBelongsToTeam("filipecosta"))
   {
-    cout << "o filipe esta na equipa certa" << endl;
+    // cout << "o filipe esta na equipa certa" << endl;
+    ROS_INFO("o filipe esta na equipa certa");
   };
 
   // ros::NodeHandle node;
-
-  ros::NodeHandle n;
 
   ros::Rate loop_rate(10);
   while (ros::ok())
